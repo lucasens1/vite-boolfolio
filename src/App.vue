e5ef56f07dc572d63394621c1fe81c04a45fe351
lucasens1 <lucasensini1@gmail.com>
2024-06-30 11:46:42 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import AppMain from './components/AppMain.vue';
+export default {
+  components : {
+    AppMain,
+  }
+}
+</script>
+
+<template>
+  <AppMain/>
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,27 @@
+<script>
+import axios from 'axios';
+export default {
+    data(){
+        return {
+            // Creo variabil che conterrà i progetti
+            projects : []
+        }
+    },
+    created(){
+        axios.get('http://127.0.0.1:8000/api/projects').then(r=> {
+            // Controllo che abbia preso correttamente i risultati dalla chiamata API
+            console.log(r.data.result);
+            this.projects = r.data.result;
+        });
+    }
+}
+</script>
+
+
+<template>
+    <h1>Main Ciao!</h1>
+</template>
+
+
+<style lang="scss">
+</style>
